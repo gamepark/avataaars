e07da9d592034dabf202e3845bf36d65dda47a3c
Romain Fromi <romain.fromi@gmail.com>
2025-03-10 09:45:30 +0100

    Add some missing exports

--- src/avatar/index.tsx
+++ src/avatar/index.tsx
@@ -1,5 +1,6 @@
 export { default } from './Avatar'
 export { default as AvatarProps } from './AvatarProps'
+export { default as SkinColor } from './SkinColor'
 export * from './clothes'
 export * from './face'
 export * from './top'
